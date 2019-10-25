99258775333fc3dbc9bbfc7a3a3d59854b2b4d0a
Nicolas Leclerc <nicolas@happinov.fr>
2019-10-25 17:51:48 +0200

    First commit.

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+
+SERVER_PORT = 3000
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.get(/^\/([^\/]*)\/?(.*)$/, function (req, res) {
+  console.log('params:',typeof req.params,req.params)
+  res.send('Hello World!')
+})
+
+app.listen(SERVER_PORT, function () {
+  console.log('Example app listening on port 3000!')
+
+  const http = require('http')
+  const options = {
+      hostname: 'localhost',
+      port: SERVER_PORT,
+      path: '/foo/bar'
+  }
+
+  http.request(options, res => {
+      console.log('status:',res.statusCode);
+  }).end()
+})
